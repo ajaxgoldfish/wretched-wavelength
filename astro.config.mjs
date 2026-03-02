220cdb54b706c653dab092b928ab36e3a45f4c1d
张宗博 <603456@163.com>
2026-03-02 16:09:43 +0800

    Remove edit link configuration from astro.config.mjs to simplify project settings.

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,9 +21,6 @@ export default defineConfig({
 					lang: 'en',
 				},
 			},
-			editLink: {
-				baseUrl: 'https://github.com/zhangzongboStarlight/wretched-wavelength/edit/main/',
-			},
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
